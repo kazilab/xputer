26a62922ccaddaa0626e052cd1c17e5cdaf46407
Kazi Uddin <67221376+kazilab@users.noreply.github.com>
2023-08-14 21:49:23 +0200

    Update __init__.py

--- xputer/__init__.py
+++ xputer/__init__.py
@@ -8,8 +8,8 @@ An XGBoost powered robust imputer.
 __author__ = 'Julhash Kazi'
 __email__ = 'xputer@kazilab.se'
 __description__ = 'xputer'
-__version__ = '0.1.1'
+__version__ = '0.2.0'
 __url__ = 'https://www.kazilab.se'
 
-from .main import Xpute, xpute
+from .main import Xpute, xpute, preprocessing_df, cnmf, run_svd
 from .gui import xgui
