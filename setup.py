b30b7b70261b82bec544ea8a55068ee7a70e7f4c
Kazi Uddin <67221376+kazilab@users.noreply.github.com>
2023-08-14 21:44:43 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ if not (sys.version_info[0] == 3 and sys.version_info[1] >= 9):
 # main setup command
 setup(
     name='xputer',
-    version='0.1.1', # Major.Minor.Patch
+    version='0.2.0', # Major.Minor.Patch
     author='Julhash Kazi',
     author_email='xputer@kazilab.se',
     url='https://www.kazilab.se',
